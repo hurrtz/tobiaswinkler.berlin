d57b34285f02abfd25231ccb6a43d4946fe41c38
Tobias Winkler <info@tobiaswinkler.berlin>
2016-03-12 23:48:49 +0100

    changes due to first build

---
+++ client/app/app.constant.js
@@ -0,0 +1,13 @@
+(function(angular, undefined) {
+  angular.module("tobiaswinklerBerlinApp.constants", [])
+
+.constant("appConfig", {
+	"userRoles": [
+		"guest",
+		"user",
+		"admin"
+	]
+})
+
+;
+})(angular);
